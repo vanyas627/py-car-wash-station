bdd93e543b94a25ab70d17944ff0b12c1e7832d7
Ivan Sotsenko <vanja.sotsenko@gmail.com>
2026-03-29 21:02:07 +0200

    Change serve_cars method

--- app/main.py
+++ app/main.py
@@ -11,14 +11,14 @@ class CarWashStation:
 
     def __init__(self, distance_from_city_center: float, clean_power: int,
                  average_rating: float, count_of_ratings: int) -> None:
-        self.distance_from_center = distance_from_city_center
+        self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
     def calculate_washing_price(self, car: Car) -> float:
         return round((car.comfort_class * (self.clean_power - car.clean_mark)
-                      * self.average_rating) / self.distance_from_center, 1)
+                      * self.average_rating) / self.distance_from_city_center, 1)
 
     def wash_single_car(self, car: Car) -> None:
         if self.clean_power > car.clean_mark:
@@ -30,7 +30,7 @@ class CarWashStation:
             if self.clean_power > car.clean_mark:
                 income += self.calculate_washing_price(car)
                 self.wash_single_car(car)
-        return income
+        return round(income, 1)
 
     def rate_service(self, rating: int) -> None:
         result = round(((self.average_rating * self.count_of_ratings)
